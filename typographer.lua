ab18d3c33b7bc1d96f2869c7b486b79b7a00c44a
Dei Layborer <deilayborer@pm.me>
2024-05-20 13:22:19 -0400

    Update typographer.lua
    - Enable a local (buffer-specific) preference for whether the plugin should active
    - Refactored to pull replacement pairs from a table
    - Added basic logic to determine whether there's only one possible replacement or two

--- typographer.lua
+++ typographer.lua
@@ -1,49 +1,80 @@
 VERSION = "0.0.1"
 
+---- IMPORTS ----
+local micro = import("micro")
 local mutil = import("micro/util")
 local config = import("micro/config")
 local buffer = import("micro/buffer")
 
+---- CONFIG ----
+local active                -- whether typographer is active on the current bufPane
+local autoclose             -- whether autoclose is active on the current bufPane
+local autoclosePairs = {"\"\"", "''", "``", "()", "{}", "[]"}
+local typographerReplacements = {
+    ["\""] = "“”", 
+    ["'"] = "‘’",
+}
 
--- all credit to hltrailingws on GitHub for this function
+-- stolen from the (official) autoclose plugin
 function charAt(str, index)
-    return mutil.RuneAt(str, index - 1) -- lua is 1-indexed
+    return mutil.RuneAt(str, index - 1) -- Lua's indexing is 1 off of Go's
 end
 
 
---[[ -----------------------------------------------------------
-    When a rune is inserted, check to see what it is,
-    what's around it (i.e. runes on either side),
-    and whether it needs to be replaced with anything
-    -----------------------------------------------------------
---]] 
+function onBufPaneOpen(bp)
+
+    active = bp.Buf.Settings["typographer.active"]
+    autoclose = bp.Buf.Settings["autoclose"]
+
+end
+
 function onRune(bp, rn)
 
-    if rn == "'" then
-        local curLine = bp.Buf:Line(bp.Cursor.Y)
+    if active == false then return end
 
-        -- check "previous" character
-        -- have to actually go back 2, since the one we're replacing has been inserted at this point
-        local prevChar = charAt(curLine, bp.Cursor.X - 1)
-
-        if prevChar == " " or prevChar == "" then
-            bp:Backspace()
-            bp.Buf:Insert(-bp.Cursor.Loc, "‘")
-        else
-            bp:Backspace()
-            bp.Buf:Insert(-bp.Cursor.Loc, "’")
-        end
-    elseif rn == "\"" then
-        local curLine = bp.Buf:Line(bp.Cursor.Y)    -- text of the current line, not its index
-
-        local prevChar = charAt(curLine, bp.Cursor.X - 1)
-
-        if prevChar == " " or prevChar == "" then
-            bp:Backspace()
-            bp.Buf:Insert(-bp.Cursor.Loc, "“")
-        else
-            bp:Backspace()
-            bp.Buf:Insert(-bp.Cursor.Loc, "”")
+    -- check to see if the rune that was entered is one the plugin replaces
+    for key, val in pairs(typographerReplacements) do
+        if rn == key then
+            replaceRune(bp, rn, val)
+            break
         end
     end
+end
+
+function replaceRune(bp, enteredRune, replacementRune)
+
+    -- temporarily disable autoclose if needed
+    if autoclose then
+        bp.Buf:SetOption("autoclose", "false")
+    end
+
+    -- check to see how many replacement possibilities there are
+    if charAt(replacementRune, 3) == "" then       -- two
+        micro.InfoBar():Message("replacing")
+        local leftChar = charAt(replacementRune, 1)
+        local rightChar = charAt(replacementRune, 2)
+        
+        -- see if we need the left character or the right.  Assumes that left begins a word.
+        local curLine = bp.Buf:Line(bp.Cursor.Y)
+        local prevChar = charAt(curLine, bp.Cursor.X - 1)
+        bp:Backspace()
+        if prevChar == " " or prevChar == "" then
+            bp.Buf:Insert(-bp.Cursor.Loc, leftChar)
+        else
+            bp.Buf:Insert(-bp.Cursor.Loc, rightChar)
+        end
+    end
+
+    -- re-enable autoclose if needed
+    if autoclose then
+        bp.Buf:SetOption("autoclose", "true")
+    end
+
+end
+
+
+function init()
+    config.RegisterCommonOption("typographer", "active", true)
+    config.RegisterCommonOption("typographer", "avoidautoclose", true)
+
 end
